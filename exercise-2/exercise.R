cfa29e1154a03b84ec0b647e20aa4f70ac751e6c
RYAN MILLS <rmills95@uw.edu>
2016-11-29 15:09:09 -0800

    Finished Exercise-2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,5 +1,7 @@
 # Introductory example using the housing data used here: 
 # http://www.r2d3.us/visual-intro-to-machine-learning-part-1/
+install.packages("rpart")
+install.packages("rpart.plot")
 library(rpart)
 library(rpart.plot)
 
@@ -32,24 +34,26 @@ for(i in 1:100) {
   # Create test and training data
   # Hint: http://stackoverflow.com/questions/17200114/how-to-split-data-into-training-testing-sets-using-sample-function-in-r-program
   # 1. Create training and testing datasets by sampling 75% of your data from your `homes` dataframe.
-  
+  train.num <- sample(seq_len(nrow(homes)), size = sample.size)
+  train.data <- homes[train.num,]
+  test.data <- homes[-train.num,]
   
   # 2. Pass your **training data** to the `rpart` function to run a simple classification operation
-  
+  basic.fit <- rpart(in_sf ~ ., data = train.data, method = "class")
   
   # 3. Pass your results to the `AssessFit` function to assess the fit
-  
+  assessment <- AssessFit(basic.fit, data= test.data)
   
   # 4. Store your assessment in the `basic.fits` vector
-  
+  basic.fits = c(basic.fits, assessment)
 }
 
 # 5. Make a histogram of your `basic.fits` vector
-
+hist(basic.fits)
 
 # 6. Take the mean of your `basic.fits` vector
-
+mean(basic.fits)
 
 
 # 7. Pass your most recent model to the `rpart.plot` function to graph it
-
+rpart.plot(basic.fit)
